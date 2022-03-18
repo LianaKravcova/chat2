d575267a7bd0ce25a48234a649ba049dd0241365
Liana <lianasuhanova88@gmail.com>
2022-03-18 15:20:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const API = "https://chat2022.lianasuhanova88.repl.co"
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
 
 function sutitZinu()
 {
-    ///console.log('sutitZinu() darbojas');
+    console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML +'<br />' + zina.value;
+
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value)
 }
 
 async function ieladetChataZinas()
